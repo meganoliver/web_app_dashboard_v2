25bc406444685f9380e1e0317452b208a9936803
pdxoliver <rms.moliver@gmail.com>
2017-08-24 09:59:28 -0700

    Added pop up notification box.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
+const notifyBubble = document.getElementById('notify-bell');
+const overlay = document.getElementById('alert-overlay');
+const alertBox = document.getElementById('alert-box');
+const closeAlert = document.getElementById('close');
+const hide = document.getElementById('alert');
+const alertCircle = document.getElementById('svg-alert');
+const popUpClose = document.getElementsByClassName('alert-close');
+const alertMessage = document.getElementsByClassName('pop-up-alert');
+
+
 //Create a circle svg
+function g () {
+  createG = d3.select('.bell').append('g')
+    .attr('id', 'notify')
+    .attr('width', '16')
+};
+
+g()
+
 function notificationBubble() {
-  createCircle = d3.select('.bell').append('circle')
+  createCircle = d3.select('#notify').append('circle')
     .attr('id', 'svg-alert')
     .attr('r', '8')
     .attr('cx', '25')
@@ -11,13 +29,46 @@ function notificationBubble() {
 
 notificationBubble()
 
+function number() {
+  createText = d3.select('#notify').append('text')
+    .attr('id', 'alert-number')
+    .attr('x', '25')
+    .attr('y', '4')
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'black')
+    .attr('font-size', '.7rem')
+    .attr('font-weight', 'bold')
+    .text(2)
+};
 
-//Close alert box and circle on click
-const closeAlert = document.getElementById('close');
-const hide = document.getElementById('alert');
-const alertCircle = document.getElementById('svg-alert')
+number()
+
+//Close alert box on click
 
 closeAlert.addEventListener('click', () => {
   hide.style.visibility = 'hidden';
-  alertCircle.style.display = 'none';
+  alertCircle.style.visibility = 'hidden';
 });
+
+//Create alert box on notification bubble click
+
+alertBox.className = 'hidden';
+overlay.className = 'hidden';
+
+notifyBubble.addEventListener('click', (e) =>{
+    alertBox.className = 'visible';
+    overlay.className = 'visible';
+});
+
+//Close alert messages when x is clicked
+for(let i = 0; i < popUpClose.length; i++) {
+  popUpClose[i].addEventListener('click', function(e) {
+    alertBox.removeChild(popUpClose[i]);
+    alertBox.removeChild(alertMessage[i]);
+    
+     if(popUpClose.length == 0 && e) {
+      alertBox.style.visibility = 'hidden';
+      overlay.style.visibility = 'hidden';
+    }
+  });
+}
